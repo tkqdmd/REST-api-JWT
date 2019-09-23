eabbad732259c16f8e60a381d79fc58c52afca30
Thao <thaptam0@gmail.com>
2019-09-23 11:13:48 +0700

    demo complete

---
+++ src/main/java/com/example/demo/rest/RestAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package com.example.demo.rest;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public final class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+//		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Unauthorized");
+    }
+}
